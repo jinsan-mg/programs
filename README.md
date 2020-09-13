3224a122514dd47c3efd15a31fd0430332eb158b
Jinsan <jinsgeorge@paypal.com>
2020-09-13 11:27:43 +0530

    updated the variable names, test cases and readme

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ a program to create a palindrome string based on the user input.
 eg: word length - 4, number of chars - 2, number of digits - 2, number of special chars - 0
 output: a00a;
 The program also checks for invalid use cases and prints the same.
+#methods used:
+# 1. convertToInt
+This method will convert the commandline string inputs to integer array. Also set the invalid flag to 1 if invalid inputs are passed.
+Takes string array as input and returns integer array
+# 2. checkForInvalidPalindromeInput
+Method used to validate the inputs. Check the boundary conditions for the inputs, invalid combinations of inputs and returns the flag accordingly
+# 3. buildPalindrome
+method which creates the required output. Accept the integer array as input and returns the palindrome string.
+The methods divides the x,y and z length to half to build the first half of the palindrome and combine all of them to create the first half.
+It also check if the any of x,y and z is odd and create the character to add in middle.
+Combines the first half, middle string and reverse of first half to return the final output.
+# 4. createStream
+The method create the stream of character based on the ascci value range and the length required.
--- src/sample/createPalindrom.java
+++ src/sample/createPalindrom.java
@@ -4,22 +4,16 @@ public class createPalindrom {
     static int invalid = 0;
 
     public static void main(String[] args) {
-        //String[] inp = {"10", "6", "4", "0"};
         String[] inp = args;
         if(inp.length!=4){
             invalid=1;
         }
-        int oddCount = 0;
-        //convert strings agrs to integer
-        int[] pals = convertInt(inp);
-        //check for invalid inputs
+        int[] int_Inputs = convertToInt(inp);
         int invFlag = 0;
         if(invalid==0){
-            invFlag = checkForInvalidPalindrom(pals);
+            invFlag = checkForInvalidPalindromeInput(int_Inputs);
             if(invFlag==0){
-                String palindromeString = buildPalindrome(pals);
-                //test the formed string
-                checkPalindrome(palindromeString);
+                String palindromeString = buildPalindrome(int_Inputs);
                 System.out.println(palindromeString);
             }
             else {
@@ -31,38 +25,38 @@ public class createPalindrom {
         }
     }
     static String buildPalindrome(int[] inp){
-        String strPal = "";
-        int x1 = inp[1]/2;
-        int y1 = inp[2]/2;
-        int z1 = inp[3]/2;
-        String mid = "";
-        String s1 = createStream(97,122, x1);
-        String s2 = createStream(48,57, y1);
-        String s3 = createStream(33,47, z1);
+        String strPalindrome = "";
+        int alphaLength = inp[1]/2;
+        int numbLength = inp[2]/2;
+        int specCharLength = inp[3]/2;
+        String midString = "";
+        String alphaString = createStream(97,122, alphaLength);
+        String numbString = createStream(48,57, numbLength);
+        String specCharString = createStream(33,47, specCharLength);
         if((inp[1]%2)!=0){
-            mid = "a";
+            midString = "a";
         }
         if((inp[2]%2)!=0){
-            mid = "1";
+            midString = "1";
         }if((inp[3]%2)!=0){
-            mid = "!";
+            midString = "!";
         }
-        String s = s1+s2+s3;
-        String sr = new StringBuilder(s).reverse().toString();
-        strPal = s+mid+sr;
-        return strPal;
+        String palindromeFirstHalf = alphaString+numbString+specCharString;
+        String rev_palindromeFirstHalf = new StringBuilder(palindromeFirstHalf).reverse().toString();
+        strPalindrome = palindromeFirstHalf+midString+rev_palindromeFirstHalf;
+        return strPalindrome;
     }
     static String createStream(int start, int end, int len){
-        String s = "";
+        String charStream = "";
         int j = start;
         for(int i=0; i<len; i++){
-            s = s+ Character.toString((char) j);
+            charStream = charStream+ Character.toString((char) j);
             j++;
             if(j>end){
                 j=start;
             }
         }
-        return s;
+        return charStream;
     }
     static void checkPalindrome(String S){
         String SR = new StringBuilder(S).reverse().toString();
@@ -70,7 +64,7 @@ public class createPalindrom {
             System.out.println("Created the below string, but its not a valid palindrome");
         }
     }
-    static int[] convertInt(String[] inpString){
+    static int[] convertToInt(String[] inpString){
         int[] p = new int[4];
         try {
             p[0] = Integer.parseInt(inpString[0]);
@@ -82,12 +76,11 @@ public class createPalindrom {
         }
         return p;
     }
-    static int checkForInvalidPalindrom(int[] inp){
+    static int checkForInvalidPalindromeInput(int[] inp){
         int Flag = 0;
         int wOdd = 0;
         int oddCount = 0;
         int xyz = 0;
-        //checkFor w
         if(inp[0]<1 || inp[0]>20){
             Flag=1;
             return Flag;
@@ -97,7 +90,6 @@ public class createPalindrom {
                 wOdd = 1;
             }
         }
-        //check for x,y and z
         for(int i=1; i<4; i++){
             if(inp[i]<0 || inp[i]>20){
                 Flag=1;
@@ -110,7 +102,6 @@ public class createPalindrom {
                 xyz = xyz + inp[i];
             }
         }
-        //check for total word length and odd count
         if((xyz != inp[0]) || (wOdd != oddCount)){
             Flag=1;
             return Flag;
